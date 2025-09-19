a98e764213f81263935b6a61dae5f630e598e8b4
Keez <kl.cuijpers@student.maastrichtuniversity.nl>
2025-09-19 13:50:53 +0200

    Added adding/removing customers

--- src/database/queryManager.py
+++ src/database/queryManager.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date
 from typing import List, Dict, Any, Optional
 from pony.orm import db_session, select, desc, count
 
@@ -47,14 +47,53 @@ class QueryManager:
     def get_vegan_pizzas() -> List[Pizza]:
         """Get all pizzas that are vegan (all ingredients are vegan)."""
         return Pizza.select(p for p in Pizza if all(i.type == IngredientType.Vegan for i in p.ingredients))[:]
-
+    
     @staticmethod
     @db_session
     def get_vegetarian_pizzas() -> List[Pizza]:
         """Get all pizzas that are vegetarian (all ingredients are vegan or vegetarian)."""
         return Pizza.select(p for p in Pizza if all(i.type in [IngredientType.Vegan, IngredientType.Vegetarian] for i in p.ingredients))[:]
-    
-# Calculates pizza price based on ingredient costs, margin and VAT
+
+    #TODO: Check if this has all the options needed, since there are many optional fields
+    @staticmethod
+    @db_session
+    def add_customer(username: str, email: str, password: str, birthday_order: bool, loyalty_points: int = 0, phone: Optional[str] = None, address: Optional[str] = None, postal_code: Optional[str] = None, birthdate: Optional[date] = None, gender: Optional[str] = None) -> Customer:
+        """Add a new customer to the database with all available options."""
+        customer_data = {
+            'username': username,
+            'email': email,
+            'birthday_order': birthday_order,
+            'loyalty_points': loyalty_points
+        }
+
+        # Add optional fields only if they are provided
+        if phone is not None:
+            customer_data['phone'] = phone
+        if address is not None:
+            customer_data['address'] = address
+        if postal_code is not None:
+            customer_data['postalCode'] = postal_code
+        if birthdate is not None:
+            customer_data['birthdate'] = birthdate
+        if gender is not None:
+            customer_data['Gender'] = gender
+
+        customer = Customer(**customer_data)
+        customer.set_password(password)
+        return customer
+
+    #TODO: Check if everything gets removed properly (orders, discount codes, etc.)
+    @staticmethod
+    @db_session
+    def remove_customer(username: str) -> bool:
+        """Remove a customer from the database by username."""
+        customer = Customer.get(username=username)
+        if customer:
+            customer.delete()
+            return True
+        return False
+
+    # Calculates pizza price based on ingredient costs, margin and VAT
     @staticmethod
     @db_session
     def calculate_pizza_price(pizza_id: int) -> float:
